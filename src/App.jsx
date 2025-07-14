5bf69e1599b1b374a16c034352af2638fc39d104
premreddy2103 <baddampremkumarreddy@gmail.com>
2025-07-15 00:08:25 +0530

    Saved and updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import AddProduct from "./components/AddProduct";
 import Product from "./components/Product";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./Context/Context";
+import { Routes, Route } from "react-router-dom";
 import UpdateProduct from "./components/UpdateProduct";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -21,6 +18,7 @@ function App() {
     setSelectedCategory(category);
     console.log("Selected category:", category);
   };
+
   const addToCart = (product) => {
     const existingProduct = cart.find((item) => item.id === product.id);
     if (existingProduct) {
@@ -37,26 +35,22 @@ function App() {
   };
 
   return (
-    <AppProvider>
-      <BrowserRouter>
-        <Navbar onSelectCategory={handleCategorySelect}
-         />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home addToCart={addToCart} selectedCategory={selectedCategory}
-              />
-            }
-          />
-          <Route path="/add_product" element={<AddProduct />} />
-          <Route path="/product" element={<Product  />} />
-          <Route path="product/:id" element={<Product  />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/update/:id" element={<UpdateProduct />} />
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
+    <>
+      <Navbar onSelectCategory={handleCategorySelect} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home addToCart={addToCart} selectedCategory={selectedCategory} />
+          }
+        />
+        <Route path="/add_product" element={<AddProduct />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/update/:id" element={<UpdateProduct />} />
+      </Routes>
+    </>
   );
 }
 
