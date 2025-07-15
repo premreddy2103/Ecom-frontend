dbfcc49f29adf0236c488d1fc99af3c1fb010717
premreddy2103 <baddampremkumarreddy@gmail.com>
2025-07-15 11:36:24 +0530

    Fix: Add BrowserRouter wrapper in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { useContext } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { AppProvider } from "./Context/Context.jsx";
-// import { BrowserRouter as Router } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Router> */}
+    <Router>
       <AppProvider>
         <App />
       </AppProvider>
-    {/* </Router> */}
+    </Router>
   </React.StrictMode>
 );
 
